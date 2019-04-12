840d2307689e71a05c61a9481ef3ea338ade6b43
Paul Seal <paul@moriyama.co.uk>
2019-04-12 13:58:16 +0100

    - added method to get the root node id if an ancestor of the page has a domain attached to it. - used the root node id in the redirect url if it isn't empty

--- Solution/tooorangey.RumDo/Controllers/RumDoApiController.cs
+++ Solution/tooorangey.RumDo/Controllers/RumDoApiController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using Umbraco.Core;
 using Umbraco.Core.Models;
+using Umbraco.Core.Services;
 using Umbraco.Web.WebApi;
 
 namespace tooorangey.RumDo.Controllers
@@ -13,23 +15,53 @@ namespace tooorangey.RumDo.Controllers
         public IHttpActionResult CreateRedirect(RedirectInstruction instruction)
         {
             var redirectUrlService = Services.RedirectUrlService;
+            var domainService = Services.DomainService;
+            var contentService = Services.ContentService;
+
             if (!String.IsNullOrEmpty(instruction.ContentUdi))
             {
-                GuidUdi guidIdi;
-                instruction.ContentKey = GuidUdi.TryParse(instruction.ContentUdi, out guidIdi) ? guidIdi.Guid : default(Guid);
+                instruction.ContentKey = GuidUdi.TryParse(instruction.ContentUdi, out var guidIdi) ? guidIdi.Guid : default(Guid);
             }
             try
             {
+                var rootNodeId = GetRootNodeId(instruction, domainService, contentService);
+
+                instruction.RedirectFromUrl = string.IsNullOrWhiteSpace(rootNodeId) 
+                                                ? instruction.RedirectFromUrl 
+                                                : rootNodeId + instruction.RedirectFromUrl;
 
                 redirectUrlService.Register(instruction.RedirectFromUrl, instruction.ContentKey);
+
                 return Ok();
             }
             catch (Exception ex)
             {
                 return BadRequest(ex.Message);
             }
-
         }
+
+        private string GetRootNodeId(RedirectInstruction instruction, IDomainService domainService, IContentService contentService)
+        {
+            var rootNodeId = string.Empty;
+            var domains = domainService.GetAll(true).Where(x => x.RootContentId.HasValue).ToList();
+
+            if (domains.Any())
+            {
+                var content = contentService.GetById(instruction.ContentKey);
+                if (content == null) throw new Exception("Content item does not exist");
+
+                var pathNodeIds = content.Path.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                if (pathNodeIds.Any())
+                {
+                    var assignedDomain = domains.FirstOrDefault(x => pathNodeIds.Contains(x.RootContentId?.ToString()));
+                    rootNodeId = assignedDomain?.RootContentId?.ToString() ?? string.Empty;
+                }
+            }
+
+            return rootNodeId;
+        }
+
         [HttpGet]
         public IEnumerable<IRedirectUrl> GetContentRedirectUrls(string contentUdi)
         {
